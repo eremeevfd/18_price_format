85aab8fb7d1f4efeb17904da6282a05612be3103
eremeevfd <dartfedor@mail.ru>
2017-01-10 18:09:55 +0300

    Tests for format_price function to ensure right output data.

--- tests.py
+++ tests.py
@@ -1,1 +1,21 @@
-# TODO place your tests here
+import unittest
+from format_price import format_price
+
+class TestFormatPriceMethod(unittest.TestCase):
+    def test_format_price_without_fraction(self):
+        self.assertEqual(format_price('3245.0000000'), '3 245')
+
+    def test_format_price_with_fraction(self):
+        self.assertEqual(format_price('3245.12123123123'), '3 245.12')
+
+    def test_format_price_negative(self):
+        self.assertEqual(format_price('-3'), '3')
+
+    def test_format_price_returns_None(self):
+        self.assertIsNone(format_price('asdf'))
+        self.assertIsNone(format_price('3456.775667.65'))
+
+
+
+if __name__=='__main__':
+    unittest.main()
