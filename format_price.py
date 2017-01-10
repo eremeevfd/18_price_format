1964971bab96f8142a60ba30695593dc5982ac10
eremeevfd <dartfedor@mail.ru>
2017-01-10 18:08:23 +0300

    Wrote script. Formats price. Returns None if can't format.

--- format_price.py
+++ format_price.py
@@ -1,5 +1,19 @@
+import sys
+
+
 def format_price(price):
-    pass
+    try:
+        absolute_numeric_price = abs(float(price))
+        if absolute_numeric_price.is_integer():
+            return '{:,.0f}'.format(float(absolute_numeric_price)).replace(',',' ')
+        else:
+            return '{:,.2f}'.format(float(absolute_numeric_price)).replace(',',' ')
+    except Exception:
+        return None
+
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) == 1:
+        print(format_price(input()))
+    else:
+        print(format_price(sys.argv[1]))
